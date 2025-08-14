62867b3a527a0c9dbe473a4e3ef1c8dfeba8af5c
Vivek <saigoguladinne@gmail.com>
2025-08-14 10:28:57 +0530

    trying to build connection with mongodb for user authenctication

---
+++ backend/app/models/user_model.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+from datetime import datetime, timezone
+from bson import ObjectId
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+    @classmethod
+    def validate(cls,v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid ObjectId")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls,field_schema):
+        field_schema.update(type="string")
+
+class UserModel(BaseModel):
+    id: Optional[PyObjectId] = Field(alias="_id")
+    username: str
+    email: EmailStr
+    password: str
+    created_at: Optional[datetime] = Field(default_factory = lambda: datetime.now(timezone.utc))
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+
